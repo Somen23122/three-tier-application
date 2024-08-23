862cc1d092b979f0b1a3edce449ff609098773b8
Somen23122 <somen8877@gmail.com>
2024-08-23 17:37:01 +0530

    Uploading intial commit

--- provider.tf
+++ provider.tf
@@ -10,10 +10,10 @@ terraform {
 }
 
 # Configure the Microsoft Azure Provider
-provider "azurerm" {
-  subscription_id = "05431236-577b-4c75-9f9b-058ef2431e96"
-  client_id       = "5c97a815-9792-4a1d-bb9f-5e3b24b2c20b"
-  client_secret   = "Lqg8Q~ABPRvfs_FFjgtpKzm_hrj6R1VG4WutFbJH"
-  tenant_id       = "bb97c8b6-2d78-4ec5-87f6-28a2c5f89267"
-  features {}
-}
+# provider "azurerm" {
+#   subscription_id = "05431236-577b-4c75-9f9b-058ef2431e96"
+#   client_id       = "5c97a815-9792-4a1d-bb9f-5e3b24b2c20b"
+#   client_secret   = "Lqg8Q~ABPRvfs_FFjgtpKzm_hrj6R1VG4WutFbJH"
+#   tenant_id       = "bb97c8b6-2d78-4ec5-87f6-28a2c5f89267"
+#   features {}
+# }
